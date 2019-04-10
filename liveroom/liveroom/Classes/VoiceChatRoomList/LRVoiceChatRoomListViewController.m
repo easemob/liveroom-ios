c0004a850bbee735da29d9a6dc6b3502070a5653
傅东海 <296322762@qq.com>
2019-04-10 12:25:05 +0800

    适配iPhone X

--- liveroom/liveroom/Classes/VoiceChatRoomList/LRVoiceChatRoomListViewController.m
+++ liveroom/liveroom/Classes/VoiceChatRoomList/LRVoiceChatRoomListViewController.m
@@ -67,8 +67,7 @@
     [self.view addSubview:self.titleLabel];
     [self.titleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
         make.left.equalTo(self.view).offset(13);
-        make.top.equalTo(self.view).offset(20);
-        make.height.equalTo(@60);
+        make.top.equalTo(self.view).offset(LRSafeAreaTopHeight);
     }];
     
     [self _setupSearch];
@@ -84,7 +83,7 @@
     self.searchBar.delegate = self;
     [self.view addSubview:self.searchBar];
     [self.searchBar mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(self.titleLabel.mas_bottom);
+        make.top.equalTo(self.titleLabel.mas_bottom).offset(10);
         make.left.equalTo(self.view).offset(13);
         make.right.equalTo(self.view).offset(-13);
         make.height.equalTo(@50);
@@ -102,7 +101,7 @@
         make.top.equalTo(self.searchBar.mas_bottom).offset(10);
         make.left.equalTo(self.view).offset(13);
         make.right.equalTo(self.view).offset(-13);
-        make.bottom.equalTo(self.view);
+        make.bottom.equalTo(self.view).offset(-LRSafeAreaBottomHeight);
     }];
     
     self.searchResultTableView = [[UITableView alloc] init];
@@ -186,7 +185,7 @@
                     make.top.equalTo(self.searchBar.mas_bottom).offset(10);
                     make.left.equalTo(self.view).offset(13);
                     make.right.equalTo(self.view).offset(-13);
-                    make.bottom.equalTo(self.view);
+                    make.bottom.equalTo(self.view).offset(-LRSafeAreaBottomHeight);
                 }];
             }
         }
